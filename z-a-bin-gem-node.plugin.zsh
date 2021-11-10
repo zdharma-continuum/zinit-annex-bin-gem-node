eb2c03d490e1d450a541da65d6519ab4afdb5a90
Philipp Schmitt <philipp@schmitt.co>
2021-11-10 18:27:31 +0100

    Update url

--- z-a-bin-gem-node.plugin.zsh
+++ z-a-bin-gem-node.plugin.zsh
@@ -2,7 +2,7 @@
 # License MIT
 
 # According to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
