2f4045c412efd563901d1d207b1cd49836e3b341
Vladislav Doster <mvdoster@gmail.com>
2022-07-09 05:34:11 -0500

    style: format markdown and zsh to zinit convention
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- z-a-bin-gem-node.plugin.zsh
+++ z-a-bin-gem-node.plugin.zsh
@@ -8,39 +8,39 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 autoload .za-bgn-bin-or-src-function-body \
-     .za-bgn-bin-or-src-function-body-cygwin \
-    .za-bgn-mod-function-body \
-    za-bgn-atload-handler za-bgn-atclone-handler \
-    za-bgn-atpull-handler za-bgn-help-handler \
-    za-bgn-atdelete-handler \
-    za-bgn-shim-list
+  .za-bgn-bin-or-src-function-body-cygwin \
+  .za-bgn-mod-function-body \
+  za-bgn-atload-handler za-bgn-atclone-handler \
+  za-bgn-atpull-handler za-bgn-help-handler \
+  za-bgn-atdelete-handler \
+  za-bgn-shim-list
 
 # An empty stub to fill the help handler fields
 za-bgn-null-handler() { :; }
 
 @zinit-register-annex "zinit-annex-bin-gem-node" \
-    subcommand:shim-list \
-    za-bgn-shim-list \
-    za-bgn-null-handler
+  subcommand:shim-list \
+  za-bgn-shim-list \
+  za-bgn-null-handler
 
 @zinit-register-annex "zinit-annex-bin-gem-node" \
-    hook:\!atload-50 \
-    za-bgn-atload-handler \
-    za-bgn-help-handler \
-    "fbin''|sbin|sbin''|gem''|node''|pip''|fmod''|fsrc''|ferc''" # also register new ices
+  hook:\!atload-50 \
+  za-bgn-atload-handler \
+  za-bgn-help-handler \
+  "fbin''|sbin|sbin''|gem''|node''|pip''|fmod''|fsrc''|ferc''" # also register new ices
 
 @zinit-register-annex "zinit-annex-bin-gem-node" \
-    hook:atclone-50 \
-    za-bgn-atclone-handler \
-    za-bgn-null-handler
+  hook:atclone-50 \
+  za-bgn-atclone-handler \
+  za-bgn-null-handler
 
 @zinit-register-annex "zinit-annex-bin-gem-node" \
-    hook:\%atpull-50 \
-    za-bgn-atclone-handler \
-    za-bgn-null-handler
+  hook:\%atpull-50 \
+  za-bgn-atclone-handler \
+  za-bgn-null-handler
 
 @zinit-register-annex "zinit-annex-bin-gem-node" \
-    hook:atdelete-50 \
-    za-bgn-atdelete-handler \
-    za-bgn-null-handler
+  hook:atdelete-50 \
+  za-bgn-atdelete-handler \
+  za-bgn-null-handler
 
